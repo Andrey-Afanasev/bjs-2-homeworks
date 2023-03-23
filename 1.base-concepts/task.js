9d76bc9f52d5209ed6949d486d0a89fa76d7e40c
Andrey Petrovich <andrey_petrovicha@mail.ru>
2023-03-23 20:41:13 +0700

    вторая задача и первая 23.03

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,16 +2,14 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let discriminant = b**2-4*a*c
-  if (discriminant < 0) {
-    arr = [];
-    return arr;
-  } else if (discriminant === 0) {
+  
+   if (discriminant === 0) {
     arr =[-b/(2*a)]
-    return arr;
+    
   } else if (discriminant > 0) {
     arr = [(-b + Math.sqrt(discriminant) )/(2*a), (-b - Math.sqrt(discriminant) )/(2*a)];
-    return arr;
-  };
+  }
+  return arr;
   
 }
 
