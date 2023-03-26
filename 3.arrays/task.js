89bb5a853bd7272a4e3db6d27a842c96ba3c8711
Andrey Petrovich <andrey_petrovicha@mail.ru>
2023-03-26 16:07:36 +0700

    26.03

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,20 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2.length && arr1.every((element, i) => element === arr2[i])
+
 }
 
 function getUsersNamesInAgeRange(users, gender) {
+
+    let filtret = users.filter(men => men.gender === gender);
+    let maper = filtret.map(age1 => age1.age);
+    let redused = maper.reduce((acc, items, index, maper) => {
+        acc += items;
+        if (index === maper.length - 1){
+            return acc / maper.length;
+            }
+        return acc;
+       } ,0) 
+
+       return redused
   
 }
