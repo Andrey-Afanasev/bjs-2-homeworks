cf153c3f4740694e69ea64ef181f668a8397cbd5
Andrey Petrovich <andrey_petrovicha@mail.ru>
2023-03-28 20:54:24 +0700

    otpravka 23.03

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,42 @@
-function Student(name, gender, age) {
-  
+
+function Student (name, gender, age) {
+
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+ 
 }
+
+let student1 = new Student("Andrey", "male", 28);
+let student2 = new Student("Anton", "male", 25);
+let student3 = new Student("Vlad", "male", 28);
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.setSubject = subjectName;
 }
 
+
+
 Student.prototype.addMarks = function (...marks) {
-  
+  if (Student.hasOwnProperty("marks") === true) {
+    addMarks = marks;
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (Student.HasOwnProperty("marks") !== false) {
+        getAverage = 0;
+      } else {
+        getAverage = addMarcs.reduce((acc, item, index) => {acc += item
+            if (index === addMarks.length){
+                return acc/addMarks.length} },0);
+      }
 }
 
 Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.exclude = reason;
   
 }
