5a45b3dc1f5f7eeec381302c70e978e723498a76
Andrey Petrovich <andrey_petrovicha@mail.ru>
2023-04-07 19:55:46 +0700

    7.04

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,58 @@
-﻿
+﻿function parseCount(number) {
+    if (isNaN(Number.parseFloat(number))) {
+        throw new Error("Невалидное значение");
+    } else {
+        return Number.parseFloat(number)
+    }
+}
+
+function validateCount(number){
+    try {
+        return parseCount(number)
+      } catch (err) {
+         return err
+      }
+}
+
+class Triangle {
+    constructor(dlina, shirina, visota) {
+        this.dlina = dlina;
+        this.shirina = shirina;
+        this.visota = visota;
+
+        if (dlina + shirina < visota || dlina + visota < shirina ||  shirina + visota < dlina) {
+            throw new Error("Треугольник с такими сторонами не существует");
+           }
+    }
+
+   
+
+   get perimeter() {
+    return this.dlina + this.shirina + this.visota;
+   }
+
+   get area() {
+    return +(Math.sqrt((this.perimeter/2) * (this.perimeter/2 - this.dlina) *
+     (this.perimeter/2 - this.shirina) * (this.perimeter/2 - this.visota))).toFixed(3)
+   }
+
+
+}
+
+function getTriangle (dlina, shirina, visota) {
+
+    try {
+        return new Triangle(dlina, shirina, visota)
+      } catch (err) {
+        return {
+            get perimeter() {
+                return "Ошибка! Треугольник не существует"
+              }, 
+              get area() {
+                return "Ошибка! Треугольник не существует"
+              }
+        }
+         
+      }
+
+   }
