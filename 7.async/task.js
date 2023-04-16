febadee1c1ab36207bc705697ffd56cf180875d7
Andrey Petrovich <andrey_petrovicha@mail.ru>
2023-04-16 16:50:06 +0700

    16.04

---
+++ 7.async/task.js
@@ -0,0 +1,71 @@
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [];
+        this.intervalId = null;
+
+    }
+
+    addClock(time, callback){
+
+        if ( !time || !callback) {
+            throw new Error('Отсутствуют обязательные аргументы');
+        }
+
+        if ( this.alarmCollection.some( a => a.time === time)) {
+            console.warn('Уже присутствует звонок на это же время');
+        }
+
+        this.alarmCollection.push({callback, time, canCall: true});
+
+    }
+
+    removeClock(time) {
+
+        this.alarmCollection = this.alarmCollection.filter( (a) => a.time !== time)
+
+    }
+
+    getCurrentFormattedTime() {
+        let Data = new Date();
+        let Hour = Data.getHours();
+        let Minutes = Data.getMinutes();
+
+        let hourMinut = `${Hour}:${Minutes}`
+
+        return hourMinut
+    }
+
+    start() {
+        if (this.intervalId) {
+            return
+        }
+
+        this.intervalId = setInterval(() => 
+        {this.alarmCollection.forEach((elem) => {if (elem.time === getCurrentFormattedTime()) {
+            elem.canCall = false;
+            elem.callback();
+           }
+          }
+         );
+        }, 1000)
+
+
+    }
+
+    stop() {
+        clearInterval(this.intervalId)
+        this.intervalId = null;
+    }
+
+    resetAllCalls() {
+        this.alarmCollection.forEach((elem) => elem.canCall = true)
+
+    }
+
+    clearAlarms() {
+        stop();
+        this.alarmCollection = [];
+    }
+
+
+}
